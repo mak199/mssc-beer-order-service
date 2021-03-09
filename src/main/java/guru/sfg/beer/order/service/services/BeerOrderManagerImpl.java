fc244742d5d8e6d95d7ce77d6f08b78fbdeeef24
mak199 <mak.khan199@googlemail.com>
2021-03-09 14:10:32 +0100

    Implementing Wait for SM State Change. closes #29

--- src/main/java/guru/sfg/beer/order/service/services/BeerOrderManagerImpl.java
+++ src/main/java/guru/sfg/beer/order/service/services/BeerOrderManagerImpl.java
@@ -19,7 +19,8 @@ import javax.persistence.EntityManager;
 import javax.transaction.Transactional;
 import java.util.Optional;
 import java.util.UUID;
-
+import java.util.concurrent.atomic.AtomicBoolean;
+import java.util.concurrent.atomic.AtomicInteger;
 
 
 @Slf4j
@@ -32,7 +33,8 @@ public class BeerOrderManagerImpl implements BeerOrderManager {
     private final StateMachineFactory<BeerOrderStatusEnum, BeerOrderEventEnum> stateMachineFactory;
     private final BeerOrderRepository beerOrderRepository;
     private final BeerOrderStateChangeInterceptor beerOrderStateChangeInterceptor;
-    private final EntityManager entityManager;
+
+    //private final EntityManager entityManager;
 
     @Transactional
     @Override
@@ -57,6 +59,8 @@ public class BeerOrderManagerImpl implements BeerOrderManager {
         beerOrderOptional.ifPresentOrElse(beerOrder -> {
             if(isValid){
                 sendBeerOrderEvent(beerOrder, BeerOrderEventEnum.VALIDATION_PASSED);
+
+                awaitForStatus(beerOrderId,BeerOrderStatusEnum.VALIDATED);
 
                 BeerOrder validatedOrder = beerOrderRepository.findById(beerOrderId).get();
 
@@ -141,6 +145,34 @@ public class BeerOrderManagerImpl implements BeerOrderManager {
                 .build();
 
         sm.sendEvent(msg);
+    }
+
+    private void awaitForStatus(UUID beerOrderId, BeerOrderStatusEnum statusEnum){
+        AtomicBoolean found = new AtomicBoolean(false);
+        AtomicInteger loopCount = new AtomicInteger(0);
+
+        while(!found.get()){
+            if(loopCount.incrementAndGet() > 10){
+                found.set(true);
+                log.debug("Order Found");
+            }
+            beerOrderRepository.findById(beerOrderId).ifPresentOrElse(beerOrder -> {
+                if(beerOrder.getOrderStatus().equals(statusEnum)){
+                    found.set(true);
+                    log.debug("Order Found");
+                }else{
+                    log.debug("Order Status Not Equal. Expected "+statusEnum.name()+ " Found:"+beerOrder.getOrderStatus().name());
+                }
+            },()->{log.debug("Order Id Not Found");});
+
+            if(!found.get()){
+                try{
+                    log.debug("Sleeping for retry");
+                    Thread.sleep(100);
+                }
+                catch (Exception e){}
+            }
+        }
     }
 
     private StateMachine<BeerOrderStatusEnum, BeerOrderEventEnum> build(BeerOrder beerOrder){
