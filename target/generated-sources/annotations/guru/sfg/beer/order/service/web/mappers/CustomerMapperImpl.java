b0fe1393060de2beb235244476fc96e40010ddba
mak199 <mak.khan199@googlemail.com>
2021-03-26 11:51:26 +0100

    adding credentials for Spring Cloud Config. closes #37

--- target/generated-sources/annotations/guru/sfg/beer/order/service/web/mappers/CustomerMapperImpl.java
+++ target/generated-sources/annotations/guru/sfg/beer/order/service/web/mappers/CustomerMapperImpl.java
@@ -10,7 +10,7 @@ import org.springframework.stereotype.Component;
 
 @Generated(
     value = "org.mapstruct.ap.MappingProcessor",
-    date = "2021-03-25T14:16:14+0100",
+    date = "2021-03-26T11:23:40+0100",
     comments = "version: 1.3.1.Final, compiler: javac, environment: Java 11.0.10 (Ubuntu)"
 )
 @Component
