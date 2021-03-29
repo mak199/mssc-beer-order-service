eb793f84acc180652727245f357d8bcc6199468e
mak199 <mak.khan199@googlemail.com>
2021-03-29 16:30:32 +0200

    Update BOM and add Docker file for Docker Image. closes #39

--- target/generated-sources/annotations/guru/sfg/beer/order/service/web/mappers/BeerOrderMapperImpl.java
+++ target/generated-sources/annotations/guru/sfg/beer/order/service/web/mappers/BeerOrderMapperImpl.java
@@ -19,7 +19,7 @@ import org.springframework.stereotype.Component;
 
 @Generated(
     value = "org.mapstruct.ap.MappingProcessor",
-    date = "2021-03-26T11:23:40+0100",
+    date = "2021-03-29T16:09:43+0200",
     comments = "version: 1.3.1.Final, compiler: javac, environment: Java 11.0.10 (Ubuntu)"
 )
 @Component
